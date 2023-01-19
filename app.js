1e3b807dea6ab13aabfea8e27468e4af5720521a
Leonardo B <bellucioleo@gmail.com>
2023-01-19 11:57:28 -0300

    correction.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var submitButton = document.querySelector("#app form button")
 var zipCodeField = document.querySelector("#app form input")
 var content = document.querySelector("#app main")
 
-submitButton.addEventListener("click, run")
+submitButton.addEventListener("click", run)
 
 function run(event) {
     event.preventDefault()
